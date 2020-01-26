2ba9e929fb0b823672b80446be72e622957d89c9
alejandro <alexarnd99@gmail.com>
2020-01-26 17:55:12 +0100

    feat(csv): add example about create csv file

---
+++ Ejemplos/csv/ejemploCrearCSV.py
@@ -0,0 +1,9 @@
+import csv
+
+with open('myCSV.csv', mode='w') as file:
+    writer = csv.writer(file, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+    writer.writerow(['Programming language', 'Designed by', 'Appeared', 'Extension'])
+    writer.writerow(['Python', 'Guido van Rossum', '1991', '.py'])
+    writer.writerow(['Java', 'James Gosling', '1995', '.java'])
+    writer.writerow(['C++', 'Bjarne Stroustrup', '1985', '.cpp'])
