e0be52cc17d931f132d8884cd3e547154f68160d
Roberto J. Muñoz de León <robertojmunozdeleon@gmail.com>
2020-01-30 14:34:33 +0100

    First steps into django urls

--- apiRESTDJANGO/apiRESTDJANGO/urls.py
+++ apiRESTDJANGO/apiRESTDJANGO/urls.py
@@ -13,9 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+# from django.contrib import admin
+# from django.urls import path
+
+from django.conf.urls import include, url
+from django.http import JsonResponse
+
+def xd():
+    return JsonResponse({ "Alex": "Muerto "})
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^crud/', include('crud.urls')), #this line added
+    url(r'^AlexanderAranda/', ),
 ]
